9bcc99b5496919daaaf9891778a2050ced4c5397
dguerrero20 <deividguerreropedraza08@gmail.com>
2026-05-09 21:40:01 -0500

    Cambiar Gmail SMTP a puerto 587

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,7 +4,9 @@ const nodemailer = require('nodemailer');
 const pool = require('../db/primary');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
